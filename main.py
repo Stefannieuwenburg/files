12c7335278e169cbc2ee525a3d559059a6069ce3
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-11-15 17:37:36 +0100

    commit

---
+++ main.py
@@ -0,0 +1,25 @@
+__winc_id__ = 'ae539110d03e49ea8738fd413ac44ba8'
+__human_name__ = 'files'
+
+
+import os 
+import shutil
+
+def clean_cache():
+    pass
+
+
+
+def cache_zip(zip_file, cache_dir):
+    pass
+
+
+
+def cached_files():
+    pass
+
+
+def find_password(cached_files_list):
+    pass
+
+
